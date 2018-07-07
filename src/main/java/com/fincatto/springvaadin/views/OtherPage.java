17b43583b55723a4318832beadd6f9a0578fd086
Diego Fincatto <diego@fincatto.com>
2018-07-07 12:47:13 -0300

    Ajustando estilos da pagina de selecao.

--- src/main/java/com/fincatto/springvaadin/views/OtherPage.java
+++ src/main/java/com/fincatto/springvaadin/views/OtherPage.java
@@ -7,7 +7,6 @@ import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.DataProvider;
@@ -15,18 +14,18 @@ import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "pagina", layout = TemplateColunasLayout.class)
-public class OtherPage extends Composite<VerticalLayout> implements Loggable  {
+public class OtherPage extends Composite<VerticalLayout> implements Loggable {
 
     @Autowired
     public OtherPage(UserRepository userRepository) {
         final Grid<User> grid = new Grid<>();
-        //grid.setItems(userRepository.findAll());
         grid.addColumn(User::getId).setHeader("ID");
         grid.addColumn(User::getNome).setHeader("Nome");
         grid.addColumn(User::getEmail).setHeader("Email");
         grid.setSelectionMode(Grid.SelectionMode.MULTI);
         grid.setMultiSort(true);
         grid.setPageSize(30);
+        //grid.setItems(userRepository.findAll());
         grid.setDataProvider(DataProvider.fromCallbacks(q -> userRepository.findByOffset(q.getOffset(), q.getLimit()).stream(), q -> userRepository.count()));
 
         final Dialog dialog = new Dialog();
