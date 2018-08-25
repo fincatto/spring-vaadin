906b553b71aac693bc27a5efc01d08ecdc427be6
Diego Fincatto <diego@fincatto.com>
2018-08-25 12:08:43 -0300

    Atualizado libs a atualizado estilos basicos do header.

--- src/main/java/com/fincatto/springvaadin/layouts/TemplateColunasLayout.java
+++ src/main/java/com/fincatto/springvaadin/layouts/TemplateColunasLayout.java
@@ -5,6 +5,8 @@ import com.fincatto.springvaadin.repositories.UserRepository;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Viewport;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.RouterLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -19,5 +21,18 @@ public class TemplateColunasLayout extends VerticalLayout implements RouterLayou
         this.setPadding(false);
         this.setSizeFull();
         this.add(new TopoComponent(userRepository));
+
+        final Tab tabUser = new Tab("Diego Fincatto");
+        //tabUser.setEnabled(false);
+        final Tabs tabs = new Tabs(tabUser, new Tab("Pedidos"),
+                new Tab("Notas"), new Tab("Cobranças"), new Tab("Históricos"),
+                new Tab("Tab six"), new Tab("Tab seven"), new Tab("Tab eight"),
+                new Tab("Tab nine"), new Tab("Tab ten"), new Tab("Tab eleven"),
+                new Tab("Tab twelve"), new Tab("Tab thirteen"),
+                new Tab("Tab fourteen"), new Tab("Tab fifteen"));
+        tabs.setWidth("100%");
+        //tabs.setSelectedIndex(2);
+        tabs.addClassName("header_tabs");
+        this.add(tabs);
     }
 }
