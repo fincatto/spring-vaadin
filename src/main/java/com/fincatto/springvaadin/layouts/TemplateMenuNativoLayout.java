73924a2913de2b3ee65876daff7a9c2a38b9ec11
Diego Fincatto <diego@fincatto.com>
2019-07-23 00:04:28 -0300

    Melhorado menu superior de navegacao.

--- src/main/java/com/fincatto/springvaadin/layouts/TemplateMenuNativoLayout.java
+++ src/main/java/com/fincatto/springvaadin/layouts/TemplateMenuNativoLayout.java
@@ -14,6 +14,7 @@ import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.H6;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.component.tabs.Tab;
@@ -37,10 +38,13 @@ public class TemplateMenuNativoLayout extends AppLayout implements Loggable {
     @Autowired
     public TemplateMenuNativoLayout(final UserRepository userRepository) {
         getLogger().debug("Iniciando...");
-        
-        final DrawerToggle drawerToggle = new DrawerToggle();
-        addToNavbar(drawerToggle);
-        addToNavbar(new H3("Wmix"));
+    
+        super.addToNavbar(new DrawerToggle());
+        super.addToNavbar(new H3("Wmix"));
+    
+        final HorizontalLayout horizontalLayout1 = new HorizontalLayout();
+        horizontalLayout1.setWidthFull();
+        super.addToNavbar(horizontalLayout1);
         
         final Tabs tabs = new Tabs();
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
@@ -49,30 +53,36 @@ public class TemplateMenuNativoLayout extends AppLayout implements Loggable {
             titulo.setEnabled(false);
             tabs.add(titulo, new Tab("Home"), new Tab("About"));
         }
-        addToDrawer(tabs);
+        super.addToDrawer(tabs);
         
         final MenuBar menuBar = new MenuBar();
         
         // Define menubar items
-        MenuItem project = menuBar.addItem("Project");
-        MenuItem account = menuBar.addItem("Account");
-        
-        SubMenu projectSubMenu = project.getSubMenu();
-        MenuItem users = projectSubMenu.addItem("Users");
-        MenuItem billing = projectSubMenu.addItem("Billing");
+        final MenuItem menuItemFornecedor = menuBar.addItem("Diego Fincatto");
+        //menuItemFornecedor.add(VaadinIcon.CARET_DOWN.create());
+    
+        //project.add(VaadinIcon.USER.create(), new Span("Diego Fincatto"), VaadinIcon.ARROW_DOWN.create());
+        final MenuItem account = menuBar.addItem("Banco do Brasil");
+    
+        final SubMenu projectSubMenu = menuItemFornecedor.getSubMenu();
+        final MenuItem users = projectSubMenu.addItem("Users");
+        final MenuItem billing = projectSubMenu.addItem("Billing");
         
         // Register actions to item selections
-        SubMenu usersSubMenu = users.getSubMenu();
+        final SubMenu usersSubMenu = users.getSubMenu();
         usersSubMenu.addItem("List", e -> this.getUI().ifPresent(ui -> ui.navigate(UsersPage.class)));
         usersSubMenu.addItem("Add", e -> this.getUI().ifPresent(ui -> ui.navigate(ClientPage.class)));
-        
-        SubMenu billingSubMenu = billing.getSubMenu();
+    
+        final SubMenu billingSubMenu = billing.getSubMenu();
         billingSubMenu.addItem("Invoices");
         billingSubMenu.addItem("Balance Events");
         
         account.getSubMenu().addItem("Edit Profile");
         account.getSubMenu().addItem("Privacy Settings");
-        addToNavbar(menuBar);
+    
+        final HorizontalLayout horizontalLayout = new HorizontalLayout(menuBar);
+        //horizontalLayout.setAlignItems(FlexComponent.Alignment.END);
+        super.addToNavbar(horizontalLayout);
         
         for (int i = 1; i < 5; i++) {
             final H6 secao1 = new H6(String.format("Secao %s", i));
@@ -83,7 +93,7 @@ public class TemplateMenuNativoLayout extends AppLayout implements Loggable {
             final VerticalLayout layout = new VerticalLayout(secao1, home, about, cliente);
             layout.setSpacing(false);
             layout.setMargin(false);
-            addToDrawer(layout);
+            super.addToDrawer(layout);
         }
     }
 }
